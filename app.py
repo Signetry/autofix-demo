05775959331ea7eb702eef9bef1fa5a6d670f4fe
t <t@t>
2026-07-30 15:23:13 -0400

    vulnerable fixture (SQLi in app.py)

---
+++ app.py
@@ -0,0 +1,13 @@
+import sqlite3
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/user")
+def get_user():
+    user_id = flask.request.args.get("id")
+    conn = sqlite3.connect("app.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE id = '%s'" % user_id)
+    return str(cur.fetchall())
